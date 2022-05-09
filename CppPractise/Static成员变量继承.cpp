5d21f90961d3b147070779be84c5478a11274fa4
Matrix <2513084541@qq.com>
2022-05-09 21:58:05 +0800

    VirtualDerivedAndVirtualTable

--- CppPractise/Static成员变量继承.cpp
+++ CppPractise/Static成员变量继承.cpp
@@ -1,11 +1,11 @@
-﻿#include "ClassTest.h"
-
-int main()
-{
-	BaseClass   foo;
-	foo.mStaticNum = 9999;
-	ChildClass  bar;
-	bar.printClassInfo();
-
-	return 0;
-}
+//#include "ClassTest.h"
+//
+//int main()
+//{
+//	BaseClass   foo;
+//	foo.mStaticNum = 9999;
+//	ChildClass  bar;
+//	bar.printClassInfo();
+//
+//	return 0;
+//}

b7189558b01426510cce952223c1126c55c83e85
Matrix <2513084541@qq.com>
2022-05-20 13:37:13 +0800

    嵌套命令空间测试

--- CppPractise/内存对齐函数.cpp
+++ CppPractise/内存对齐函数.cpp
@@ -1,27 +1,27 @@
-#include "ClassTest.h"
-
-
-//内存对齐
-template< class T > inline T Align(const T Ptr, unsigned int Alignment)
-{
-	return (T)(((unsigned int)Ptr + Alignment - 1) & ~(Alignment - 1));
-}
-
-int main()
-{
-	//unsigned int PageSize = 0x80000000 - 1;
-
-	//cout << "PageSize = " << PageSize << endl;
-	//cout << "~PageSize = " << ~PageSize << endl;
-
-	ChildClass cClass = ChildClass();
-	BaseClass   bClass = BaseClass();
-	//cClass = bClass;
-	//bClass = cClass;
-
-	bClass.PrintInfo();
-	cClass.baseFunction();
-	return 0;
-}
-
-
+//#include "ClassTest.h"
+//
+//
+////内存对齐
+//template< class T > inline T Align(const T Ptr, unsigned int Alignment)
+//{
+//	return (T)(((unsigned int)Ptr + Alignment - 1) & ~(Alignment - 1));
+//}
+//
+//int main()
+//{
+//	//unsigned int PageSize = 0x80000000 - 1;
+//
+//	//cout << "PageSize = " << PageSize << endl;
+//	//cout << "~PageSize = " << ~PageSize << endl;
+//
+//	ChildClass cClass = ChildClass();
+//	BaseClass   bClass = BaseClass();
+//	//cClass = bClass;
+//	//bClass = cClass;
+//
+//	bClass.PrintInfo();
+//	cClass.baseFunction();
+//	return 0;
+//}
+//
+//
